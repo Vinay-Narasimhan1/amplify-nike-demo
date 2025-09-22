3b62b2fc10b574931f189f1a6f8048c0fad4d964
Vinay-Narasimhan1 <vinayn2020@gmail.com>
2025-09-22 15:39:46 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "../styles/globals.css";
 import { CartProvider } from "../context/CartContext";
+import { Toaster } from "react-hot-toast";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
       <Component {...pageProps} />
+      <Toaster position="top-right" />
     </CartProvider>
   );
 }
-
