4efa271ee246f732ab0ecb3df0c6bb6b7a9d0b9e
Vinay-Narasimhan1 <vinayn2020@gmail.com>
2025-09-22 14:35:51 -0700

    Create cart.js

---
+++ pages/cart.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar";
+
+export default function Cart() {
+  return (
+    <>
+      <NavBar />
+      <main className="max-w-4xl mx-auto px-6 py-12">
+        <h1 className="text-3xl font-bold mb-6">Cart</h1>
+        <p>Your cart UI will go here.</p>
+      </main>
+    </>
+  );
+}
